ab7df160ab0061f885394b3715560d301ecfc896
Santi-Corrales <santi.developer@hotmail.com>
2024-11-14 16:48:37 +0100

    Ejercicio_entorns
    Enunciado

---
+++ src/practica1/practica1_entorns.java
@@ -0,0 +1,28 @@
+package practica1;
+
+public class practica1_entorns {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		
+		//Integrants: Santi(suma), Eric (resta), Quim(multiplicación)
+		
+		int[] sumArray = {4,36,4,23,867};
+		int[] subArray = {785675, 56, 5675, 56544, 234};
+		int[] prodArray = {6, 8, 20, 23, 12};
+		
+		// User 1 fer la suma dels elements de sumArray
+		
+	
+		
+		
+		// User 2 fer la resta dels elements de subArray
+		
+		
+		
+		// User 3 fer la multiplicació dels elements de prodArray
+		
+	}
+
+}
